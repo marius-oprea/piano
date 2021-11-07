2afbc52d9ba920df50c052a191c76b800825dfff
Marius Oprea <marius.oprea@orange.com>
2021-11-07 02:42:09 +0200

    optimized app component

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -1,25 +1,19 @@
 import styles from './app.component.scss';
-import * as MyTemplate from './app.component.html';
+import * as template from './app.component.html';
 
 export class AppComponent extends HTMLElement {
   constructor() {
-    console.log('AppComponent');
     super();
 
     const shadowRoot = this.attachShadow({mode: 'open'});
+    const appComponentStyles = document.createElement('style');
+    appComponentStyles.textContent = styles;
 
-    const t = MyTemplate;
+    const appComponentTemplate = document.createElement('div');
+    appComponentTemplate.innerHTML = template.default;
 
-    const style = document.createElement('style');
-
-    console.log(styles);
-    style.textContent = styles;
-
-    const template = document.createElement('div');
-    template.innerHTML = t.default;
-    template.style.textContent = style;
-    shadowRoot.appendChild(template);
-    shadowRoot.appendChild(style);
+    shadowRoot.appendChild(appComponentTemplate);
+    shadowRoot.appendChild(appComponentStyles);
   }
 }
 customElements.define('app-root', AppComponent);
